e2cddd2cfde70ef850f6dc6c99064e111b4ff0c3
nmaboge <nathan.maboge@student.uclouvain.be>
2025-05-13 18:38:28 +0200

    maj

--- letterboxd_scraper.py
+++ letterboxd_scraper.py
@@ -57,9 +57,19 @@ class LetterboxdScraper:
     def _setup_driver(self):
         if not self.driver:
             try:
-                service = Service(ChromeDriverManager().install())
-                self.driver = webdriver.Chrome(service=service, options=self.options)
+                chrome_options = Options()
+                chrome_options.add_argument('--headless')
+                chrome_options.add_argument('--no-sandbox')
+                chrome_options.add_argument('--disable-dev-shm-usage')
+                chrome_options.add_argument('--disable-gpu')
+                chrome_options.add_argument('--disable-setuid-sandbox')
+                chrome_options.add_argument('--single-process')
+                chrome_options.binary_location = "/usr/bin/chromium-browser"
+                
+                service = Service()
+                self.driver = webdriver.Chrome(service=service, options=chrome_options)
                 self.driver.set_page_load_timeout(30)
+                
                 # Ajouter des variables JavaScript pour masquer l'automatisation
                 self.driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
                     'source': '''
